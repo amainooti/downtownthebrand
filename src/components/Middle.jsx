59c7acc21e6eb2215aa3a2f362fc0617067aa2d5
amainooti <amainooti@gmail.com>
2022-04-15 23:11:32 +0100

    First Commit

--- src/components/Middle.jsx
+++ src/components/Middle.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import "./Middle.css";
+import Jonzing from "../assets/images/jonzing.PNG";
+import monster from "../assets/images/Monster Logo.png";
+import Impero from "../assets/images/Impero Logo.png";
+import Vibes from "../assets/images/Vibes on Vibes Logo.png";
+import Redbull from "../assets/images/redbull.PNG";
 
 function Middle() {
   return (
     <div className='container'>
         <div className="image-wrapper">
-            <img src="images/redbull.PNG" alt="" />
-            <img className='monster' src="images/Monster Logo.png" alt="" />
-            <img src="images/jonzing.png" alt="" />
-            <img className='impero' src="images/Impero Logo.png" alt="" />
+            <img src= {Redbull} alt="" />
+            <img className='monster' src={monster} alt="" />
+            <img src={Jonzing} alt="" />
+            <img className='impero' src={Impero} alt="" />
             {/* <img src="images/KE.png" alt="" /> */}
-            <img className='vibes-on-vibes' src="images/Vibes on Vibes Logo.png" alt="" />
+            <img className='vibes-on-vibes' src={Vibes} alt="" />
         </div>
     </div>
   )
